7fc053f0bd8436554474940d325f10d5957ef901
Skie <skielynn.1105945@gmail.com>
2023-01-07 09:43:02 -0800

    changes

---
+++ src/Components/IdeasForm.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function IdeasForm (props) {
+   let [input, setinput] = useState('')
+
+   function handleSubmit(e) {
+      e.preventDefault()
+      props.addIdeas(input)
+   }
+  return(
+    <form onSubmit={handleSubmit}>
+        <input type="text" onChange={e => setInput(e.target.value)} />
+        <button type="submit">Submit your idea!</button>
+    </form>
+  )
+}
+
+export default IdeasForm
